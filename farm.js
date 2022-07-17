2e9db2f48d75816a11e9adad0493a1a6bc11e8d9
CarlosCubNed <97036823+carloscfgos1980@users.noreply.github.com>
2022-07-17 07:00:55 +0200

    Add files via upload

---
+++ farm.js
@@ -0,0 +1,19 @@
+const corn = {
+    name: "corn",
+    yield: 30,
+};
+
+const price = corn.map((item) => {
+    return item.name
+})
+
+console.log(price)
+
+// const getYieldForPlant = (items) => {
+
+//     items.map = ((item) => {
+//         return item.name
+
+//     });
+// }
+
