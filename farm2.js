0480b5e836c7edf75712ace4d39deb6df7feb770
CarlosCubNed <kuva5008@gmail.com>
2022-07-23 08:18:27 +0200

    crop revenue and profit

--- farm2.js
+++ farm2.js
@@ -1,14 +1,51 @@
-
+const banana = {
+    name: "banana",
+    yield: 2,
+    cost: 3,
+    price: 5,
+};
+const input = {
+    crop: banana,
+    numCrops: 50,
+};
 const getCostsForCrop = (items) => {
-    [items].forEach((item) => {
-        cost = item.cost;
-        console.log("Cost of corn is:", cost);
-    });
-    return cost;
+    const getCostItem = (items) => {
+        [items].forEach((item) => {
+            cost = item.crop.cost;
+        });
+        return cost;
+    }
+    const itemCost = getCostItem(items);
+
+    const cropNum = (items) => {
+        [items].forEach((item) => {
+            amount = item.numCrops;
+        });
+        return amount;
+    };
+    const cropAmount = cropNum(items);
+    cropCost = itemCost * cropAmount
+    return cropCost
 }
 //getCostsForCrop(corn);
 
 const getRevenueForCrop = (items) => {
+
+    const getYield = (items) => {
+        [items].forEach((item) => {
+            itemYield = item.crop.yield;
+        });
+        return itemYield;
+    }
+    const cropYield = getYield(items);
+
+    const getPrice = (items) => {
+        [items].forEach((item) => {
+            price = item.crop.price;
+        });
+        return price;
+    }
+    const cropPrice = getPrice(items);
 
     const cropNum = (items) => {
         [items].forEach((item) => {
@@ -18,23 +55,26 @@ const getRevenueForCrop = (items) => {
     };
     const cropAmount = cropNum(items);
 
-    const getPrice = (items) => {
-        [items].forEach((item) => {
-            itemYield = item.crop.price;
-        });
-        return itemYield;
-    }
 
-    const cropYield = getPrice(items);
-
-    cropRevenue = cropAmount * cropYield;
+    cropRevenue = cropPrice * cropYield * cropAmount;
+    console.log("Revenue of Banana crop is", cropRevenue)
     return cropRevenue
 
 }
+//getRevenueForCrop(input);
 
-
+const getProfitForCrop = (items) => {
+    const itemRevenue = getRevenueForCrop(items);
+    const itemCost = getCostsForCrop(items);
+    console.log("Cost of growing banana is:", itemCost)
+    itemProfit = itemRevenue - itemCost
+    console.log("The profit of the banana crop is", itemProfit)
+    return itemProfit
+}
+//getProfitForCrop(input);
 
 module.exports = {
     getCostsForCrop,
-    getRevenueForCrop
+    getRevenueForCrop,
+    getProfitForCrop
 };
--- farm2.test.js
+++ farm2.test.js
@@ -1,15 +1,20 @@
-const { getCostsForCrop, getRevenueForCrop } = require("./farm2");
+const { getCostsForCrop, getRevenueForCrop, getProfitForCrop
+} = require("./farm2");
 
 describe("getCostsForCrop", () => {
     const banana = {
         name: "banana",
-        yield: 30,
+        yield: 2,
         cost: 3,
         price: 5,
     };
+    const input = {
+        crop: banana,
+        numCrops: 50,
+    };
 
     test("Get the cost for corn", () => {
-        expect(getCostsForCrop(banana)).toBe(3);
+        expect(getCostsForCrop(input)).toBe(150);
     });
 });
 
@@ -17,7 +22,7 @@ describe("getRevenueForCrop", () => {
     test("Get money obtein for the banana crop", () => {
         const banana = {
             name: "banana",
-            yield: 30,
+            yield: 2,
             cost: 3,
             price: 5,
         };
@@ -25,6 +30,50 @@ describe("getRevenueForCrop", () => {
             crop: banana,
             numCrops: 50,
         };
-        expect(getRevenueForCrop(input)).toBe(250);
+        expect(getRevenueForCrop(input)).toBe(500);
     });
 });
+
+describe("getProfitForCrop", () => {
+    test("Get profit of banana crop", () => {
+        const banana = {
+            name: "banana",
+            yield: 2,
+            cost: 3,
+            price: 5,
+        };
+        const input = {
+            crop: banana,
+            numCrops: 50,
+        };
+        expect(getProfitForCrop(input)).toBe(350);
+    });
+});
+
+// describe("getTotalYield", () => {
+//     test("Calculate total yield with multiple crops", () => {
+//         const banana = {
+//             name: "banana",
+//             yield: 2,
+//             cost: 3,
+//             price: 5,
+//         };
+//         const corn = {
+//             name: "corn",
+//             yield: 3,
+//             cost: 2,
+//             price: 3,
+//         };
+//         const pumpkin = {
+//             name: "pumpkin",
+//             yield: 4,
+//             cost: 1,
+//             price: 1.5,
+//         };
+//         const crops = [
+//             { crop: corn, numCrops: 5 },
+//             { crop: pumpkin, numCrops: 2 },
+//         ];
+//         expect(getTotalYield({ crops })).toBe(23);
+//     });
+// });
